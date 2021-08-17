573233192ab779b8b59000b830f7d212dd2e1dde
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2021-08-17 13:16:24 +0400

    final static

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>newsroom</p>
+        <p>Investors</p>
+        Airbnb Plus
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Accessibility</p>
+        <p>this is not a real site</p>
+        <p>it is a pretty awesome clone</p>
+        <p>Referals accepted</p>
+        <p>KingMuffy</p>
+      </div>
+      <div className=" space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>KingMuffy</p>
+        <p>Presents</p>
+        <p>skilss that will help you</p>
+      </div>
+      <div className=" space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Trust me</p>
+        <p>For the Win</p>
+        <p>we can do this..!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 import Tesi from '../components/Tesi'
+import Footer from '../components/Footer'
 
 export default function Home({ exploreData, cardData }) {
   return (
@@ -46,7 +48,16 @@ export default function Home({ exploreData, cardData }) {
             <Tesi />
           </div>
         </section>
+        <section>
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="the Greatest Outdoor"
+            description="Wishlists curated by Airbnb"
+            buttonText="Get Inspired"
+          />
+        </section>
       </main>
+      <Footer />
 
       {/* RIGHT */}
 
